f544a85289e9f795edc0046916e764d40984211f
Mas Adi <githubmasadi@gmail.com>
2025-03-10 10:04:46 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,12 @@
+def tambah():
+    angka1 =  float(input("Masukkan angka pertama: "))
+    angka2 =  float(input("Masukkan angka kedua: "))\
+    
+    hasil = angka1 + angka2
+
+    print(f"a, {angka1}")
+    print(f"b, {angka2}")
+
+    print(f"jumlah: {hasil}")
+
+tambah()
